1ad971410bb8f6b842b050543854189ff93aa9b8
wasupon <wasupon.mail@gmail.com>
2024-07-01 11:32:24 +0900

    fix: remove red border

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import SearchMovies from "./SearchMovies";
 
 export default function App() {
   return (
-    <div className="flex justify-center border border-red-500">
+    <div className="flex justify-center border ">
       {" "}
-      <div className=" mt-10 p-10 border border-red-500 w-screen max-w-[1000px]">
-        <h1 className="capitalize text-5xl  text-center font-bold">
+      <div className=" mt-10 p-10 border  w-screen max-w-[1000px]">
+        <h1 className="text-5xl font-bold text-center capitalize">
           react movie search
         </h1>
         <SearchMovies />
